198bf986510fb7087ac13368e4b1562c5994d369
Godswill Kalu <messagelordwill@gmail.com>
2023-05-01 22:15:33 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""
-This module contains endpoint(route) status
-"""
-from models import storage
-from flask import Flask
-from api.v1.views import app_views
+"""Retrieves number for each type"""
 from flask import jsonify
+from models import storage
+from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """
-    Returns a JSON status
-    """
-    return jsonify({"status": "OK"})
+    """Returns status"""
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Returns stats"""
     return jsonify({"amenities": storage.count("Amenity"),
                     "cities": storage.count("City"),
                     "places": storage.count("Place"),
